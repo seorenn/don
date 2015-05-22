85b50708581a8f7747bf42e476c28edb77aa5f5a
Heeseung Seo <hirenn@RennIP-2.local>
2015-05-22 17:01:54 +0900

    Add feature that refresh all exchange rates when document loaded

--- js/exchg.js
+++ js/exchg.js
@@ -4,14 +4,21 @@ var currencies = [
   { name: 'EUR', desc: 'Euro' },
   { name: 'JPY', desc: 'Japanese Yen' },
   { name: 'CNY', desc: 'Chinese Yuan' },
-  { name: 'RUB', desc: 'Russian Rouble' },
-  { name: 'CAD', desc: 'Canadian Dollar' },
-  { name: 'HKD', desc: 'Hong Kong Dollar' },
-  { name: 'SGD', desc: 'Singapore Dollar' },
-  { name: 'ZWD', desc: 'Zimbabwe Dollar' } // Just joke :-p
+  //{ name: 'RUB', desc: 'Russian Rouble' },
+  //{ name: 'CAD', desc: 'Canadian Dollar' },
+  //{ name: 'HKD', desc: 'Hong Kong Dollar' },
+  //{ name: 'SGD', desc: 'Singapore Dollar' },
+  //{ name: 'ZWD', desc: 'Zimbabwe Dollar' } // Just joke :-p
 ];
 
+var currencies_cache = {};
+
 var exchg_yahoo = function(from, to, complete) {
+  if (from in currencies_cache) {
+    complete(currencies_cache[from]);
+    return;
+  }
+
   var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22'
           + from
           + to
@@ -19,11 +26,17 @@ var exchg_yahoo = function(from, to, complete) {
   $.ajax({url: url})
   .done(function(data) {
     var rate = data['query']['results']['rate']['Rate']
+    currencies_cache[from] = rate;
     complete(rate);
   });
 };
 
 var exchg_wsx = function(from, to, complete) {
+  if (from in currencies_cache) {
+    complete(currencies_cache[from]);
+    return;
+  }
+
   var url = 'http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=' + from + '&ToCurrency=' + to;
   // TODO: Implement if needed...
 };
